83ee83afec84e49cf83727fcfbb8b1bf877e8564
super-rogatory <chukwudi10@gmail.com>
2019-10-21 17:53:35 -0700

    Bouncing Ball Folder Start

---
+++ BouncingBall/BouncingBallScreen/BouncingBallScreen/BouncingBallMain.cs
@@ -0,0 +1,19 @@
+﻿/********************************************************
+ * Author: Chukwudi Ikem
+ * Email: godofcollege43@csu.fullerton.edu
+ * Course: CPSC 223N
+ * Semester: Fall 2019
+ * Assignment #: 4
+ * Program name: BouncingBallScreen
+*****************************************************/
+using System;
+namespace BouncingBallScreen
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BouncingBall/BouncingBallScreen/BouncingBallScreen/BouncingBallUI.cs
@@ -0,0 +1,22 @@
+﻿/********************************************************
+ * Author: Chukwudi Ikem
+ * Email: godofcollege43@csu.fullerton.edu
+ * Course: CPSC 223N
+ * Semester: Fall 2019
+ * Assignment #: 4
+ * Program name: BouncingBallMain
+*****************************************************/
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace BouncingBallScreen
+{
+    public class BouncingBallUI : Form
+    {
+        public BouncingBallUI()
+        {
+            Size = new Size(1920, 1080);
+            Text = "Bouncing Ball in Blistering Biome";
+        }
+    }
+}
