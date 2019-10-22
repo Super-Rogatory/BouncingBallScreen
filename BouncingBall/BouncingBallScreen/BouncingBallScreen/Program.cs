c7877d44fd84730bbc98cb85364298b36a67b9a8
super-rogatory <chukwudi10@gmail.com>
2019-10-21 17:38:05 -0700

    First Commit

---
+++ BouncingBall/BouncingBallScreen/BouncingBallScreen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BouncingBallScreen
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
